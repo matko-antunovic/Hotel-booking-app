c8df8203794c84fd2e32971ca5cc56d460ce6213
matko-antunovic <47244433+matko-antunovic@users.noreply.github.com>
2019-09-19 23:15:47 +0200

    change

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import Preview from "./pages/Preview/Preview";
 import Room from "./pages/Room/Room";
 import {Route} from "react-router-dom"
 import Home from "./pages/Home/Home";
+import Nav from "./components/Nav/Nav"
+import "./App.scss"
 
 function App() {
   return (
-    <div>
+    <div className="App">
+      <Nav/>
+
+      
+    <div className="margin-wrapper">
      <Route exact path="/" component={Home}/>
      <Route exact path="/rooms" component={Preview}/>
      <Route exact path="/rooms/room/:id" component={Room}/>
+    </div>
+
     </div>
   );
 }
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./Nav.scss"
+import {NavLink} from "react-router-dom"
+
+const Nav = () => {
+    return (
+        <div className="NavBar">
+            <div className="NavBar-container">
+
+        <div className="NavBar-logo"><span>B L</span></div>
+
+                <div className="NavBar-submenus">
+                    <NavLink exact={true} to={"/"}  activeClassName="active" className="NavBar-submenus__menu">About Hotel</NavLink>
+                    <NavLink  to={"/rooms"}   activeClassName="active" className="NavBar-submenus__menu">Rooms and Suites</NavLink>
+                    <NavLink  activeClassName="active" className="NavBar-submenus__menu">Dining</NavLink>
+                    <NavLink  activeClassName="active" className="NavBar-submenus__menu">Wellness and Spa</NavLink>
+                    <NavLink  activeClassName="active" className="NavBar-submenus__menu">Gallery</NavLink>
+                </div>
+
+     <div className="NavBar-book"><span>Book your stay</span></div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Nav
